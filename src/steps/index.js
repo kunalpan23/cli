c44f26268310bc247c6bb21d2a4ed2277bd72843
Samrith Shankar <samrith.298@gmail.com>
2019-04-25 18:29:54 +0530

    Use new cwd step

--- src/steps/index.js
+++ src/steps/index.js
@@ -2,6 +2,7 @@ const Steps = require('../utils/steps.util');
 
 const createFolder = require('./create-folder');
 const addFiles = require('./add-files');
+const changeCwd = require('./change-cwd');
 const yarnInstall = require('./yarn-install');
 
 module.exports = async function() {
@@ -11,5 +12,6 @@ module.exports = async function() {
         steps.advance('Creating folder structure', 'file_folder')
     );
     await addFiles(steps.advance('Adding required files', 'page_with_curl'));
+    await changeCwd();
     await yarnInstall(steps.advance('Installing dependencies', 'package'));
 };
