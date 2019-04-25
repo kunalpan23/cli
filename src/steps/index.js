d1d0b0c4758cec62b1aa746472c6b525abc74fee
Samrith Shankar <samrith.298@gmail.com>
2019-04-25 20:01:01 +0530

    Add all steps

--- src/steps/index.js
+++ src/steps/index.js
@@ -4,14 +4,16 @@ const createFolder = require('./create-folder');
 const addFiles = require('./add-files');
 const changeCwd = require('./change-cwd');
 const yarnInstall = require('./yarn-install');
+const gitSetup = require('./git-setup');
 
 module.exports = async function() {
-    const steps = Steps.create(3);
+    const steps = Steps.create(4);
 
     await createFolder(
-        steps.advance('Creating folder structure', 'file_folder')
+        steps.advance('Landscaping your project', 'house_with_garden')
     );
-    await addFiles(steps.advance('Adding required files', 'page_with_curl'));
+    await addFiles(steps.advance('Planting the greenery', 'tulip'));
     await changeCwd();
-    await yarnInstall(steps.advance('Installing dependencies', 'package'));
+    await yarnInstall(steps.advance('Bringing in the goodies', 'gift'));
+    await gitSetup(steps.advance('Adding the finishing touches', 'sparkles'));
 };
