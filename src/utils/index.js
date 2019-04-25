3843f8face86935c40599c69a8f7e69256e4d7e9
Samrith Shankar <samrith.298@gmail.com>
2019-04-25 19:01:17 +0530

    Added name validator

--- src/utils/index.js
+++ src/utils/index.js
@@ -11,10 +11,25 @@ const readDirectory = path => fs.readdirSync(path);
 const writeFile = (path, content, opts) =>
     fs.writeFileSync(path, content, opts);
 
+const isValidName = name => {
+    if (!name) {
+        return false;
+    }
+
+    name = name.replace(/\w/g, '');
+
+    if (name && !name.includes('-') && !name.includes('_')) {
+        return false;
+    }
+
+    return true;
+};
+
 module.exports = {
     isDirectory,
     isObject,
     createDirectory,
     readDirectory,
-    writeFile
+    writeFile,
+    isValidName
 };
